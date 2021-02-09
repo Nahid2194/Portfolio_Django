1525026bb77dfe6ec679dc7d402b9accdb3bc7a0
Nahid2194 <nahidjc80@gmail.com>
2021-02-09 22:56:44 +0600

    skill model created

--- Portfolio_App/models.py
+++ Portfolio_App/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Skill(models.Model):
+    skillname = models.CharField(max_length=30)
+    skillPercent = models.IntegerField()
+
+    def __str__(self):
+        return self.skillname
