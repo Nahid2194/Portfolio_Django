036a80cced78a664d259ebd6c2b12ff79ef7284c
Nahid2194 <nahidjc80@gmail.com>
2021-02-10 09:23:58 +0600

    skill data change dynamically

--- Portfolio_App/views.py
+++ Portfolio_App/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
+from Portfolio_App.models import Skill
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'Portfolio_App/index.html', context={})
+    skills = Skill.objects.all()
+    return render(request, 'Portfolio_App/skills.html', context={'skills': skills})
